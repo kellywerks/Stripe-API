047543d1f13e76fc5730b6a7dd63534c2df8dd56
Kelly Becker & Maureen Dugan <student@epicodus.com>
2014-04-23 09:48:52 -0700

    add create/show functionality for organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,3 +1,25 @@
-class OrganizationController < ApplicationController
+class OrganizationsController < ApplicationController
 
+  def index
+    @organizations = Organization.all
+  end
+
+  def new
+    @organization = Organization.new
+  end
+
+  def create
+    @organization = Organization.create(organization_params)
+    flash[:notice] = "Organization created"
+    redirect_to organizations_path
+  end
+
+  def show
+    @organization = Organization.find(params[:id])
+  end
+
+private
+  def organization_params
+    params.require(:organization).permit(:name)
+  end
 end
--- spec/features/organization_pages_spec.rb
+++ spec/features/organization_pages_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 
-describe "create a new organization" do
+describe "organizations" do
 
   it 'should allow a user to create a new organization' do
     visit new_organization_path
     fill_in :organization_name, with: "Oregon Humane Society"
     click_button "Create Organization"
     page.should have_content "Organization created"
+  end
+
+  it 'displays an index of all organizations with links to show pages' do
+    org = Organization.create(name: "OHS")
+    org2 = Organization.create(name: "TPTA")
+    visit organizations_path
+    page.should have_content "OHS"
+    page.should have_content "TPTA"
+    click_link "TPTA"
+    page.should_not have_content "OHS"
   end
 
 end
